b2e1ec4a26c1445e8206402a1730e1c4672b48fb
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-08-04 16:20:53 +0530

    Redirections , styling added

---
+++ script.js
@@ -0,0 +1,9 @@
+// Redirection to zerodha Main Website on clicking the Zerodha Logo:
+document.getElementById("zerodha-navbar-logo").addEventListener("click" , () => {
+    window.open("https://zerodha.com/" , "_blank")
+})
+
+// Redirection to Github Repo on clicking the Menu Bar:
+document.getElementById("menu-bar-logo").addEventListener("click", () => {
+    window.location.href = "https://github.com/Shubhashish-Chakraborty/Zerodha-Landing-Page";
+});
